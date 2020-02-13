add03693b581016dff4fa992ada90f9e19d7a951
Robert Paul <robertpaul@ucla.edu>
2020-02-13 07:45:16 -0800

    update

--- src/components/Cover.jsx
+++ src/components/Cover.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Cover = () => (
 	<div className="viewport-wrap lg-width" >
-		<h1 className="home-h1">Robert Paul is a designer based in San Francisco. <br/>He tends to his plants when not working for <a href="https://quickbooks.intuit.com/" target="_blank">Intuit</a>🌱.</h1>
+		<h1 className="home-h1">Robert Paul is a designer based in Palo Alto. <br/>He tends to his plants when not working for <a href="https://quickbooks.intuit.com/" target="_blank">Intuit</a>🌱.</h1>
 	</div>
 )
 
